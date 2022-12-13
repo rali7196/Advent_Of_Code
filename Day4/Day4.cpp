82974c1370d8c2ca94065680ec23db634b1dca22
Rehan Ali <rali3@ucsc.edu>
2022-12-12 18:01:17 -0800

    Finished day4 part 1

--- Day4/Day4.cpp
+++ Day4/Day4.cpp
@@ -22,8 +22,12 @@ int main(){
 
         int startRange2 = stoi(strtok(range2, "-"));
         int endRange2 = stoi(strtok(NULL, "-"));
-
+        //check if first range contains second range
         if(startRange1 <= startRange2 && endRange1 >= endRange2){
+            counter++;
+        }
+        //check if second range contains first range
+        else if(startRange2 <= startRange1 && endRange2 >= endRange1){
             counter++;
         }
     }
