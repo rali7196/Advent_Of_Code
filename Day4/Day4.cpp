b0fe6195a17c6c9e950a04f7ee3da3d9bdb0a3de
Rehan Ali <rali3@ucsc.edu>
2022-12-12 18:03:37 -0800

    started day4 part 2

--- Day4/Day4.cpp
+++ Day4/Day4.cpp
@@ -4,6 +4,7 @@
 #include <cstring>
 
 using namespace std;
+#ifdef day1
 int main(){
     ifstream input;
     input.open("input.txt");
@@ -32,5 +33,28 @@ int main(){
         }
     }
     cout << counter << endl;
-
 }
+#else
+int main(){
+    ifstream input;
+    input.open("input.txt");
+
+    string inputLine;
+    int counter = 0;
+
+    char* cInputLine;
+    while(getline(input, inputLine)){
+        cInputLine = strdup(inputLine.c_str());
+        char* range1 = strtok(cInputLine, ",");
+        char* range2 = strtok(NULL, ",");
+
+        int startRange1 = stoi(strtok(range1, "-"));
+        int endRange1 = stoi(strtok(NULL, "-"));
+
+        int startRange2 = stoi(strtok(range2, "-"));
+        int endRange2 = stoi(strtok(NULL, "-"));
+        
+    }
+    cout << counter << endl;
+}
+#endif
